33714409037f141bc120daeb716d15791bd70e67
SedKety <nobe.mol@gmail.com>
2025-05-28 12:47:30 +0200

    Automatic boat movement
    ADDED:
    -Automatic boat movement
    -Waypoint system for the boat
    -Boat tilt

--- VR Testing/Assets/Scripts/MovementBased/BoatMovement/BoatMovement.cs
+++ VR Testing/Assets/Scripts/MovementBased/BoatMovement/BoatMovement.cs
@@ -1,35 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit.Filtering;
 
+public enum BoatMovementType
+{
+    Manual,
+    Automatic
+}
 public class BoatMovement : MonoBehaviour
 {
-    [SerializeField] private float speedMultiplier;
+    [SerializeField] private BoatMovementType movementType;
 
+    // [-
+    [Header("Shared Variables")]
+    [SerializeField] private float speedMultiplier;
+    [SerializeField] private float turnSpeed;
+    [SerializeField] private float rotationZSpeed;
+    [SerializeField] private float minZClamp, maxZClamp;
+    private Rigidbody _rb;
+    // -]
+
+    // [-
+    [Header("Manual Variables")]
     [SerializeField] private float accelerationSpeed;
     [SerializeField] private float decelerationSpeed;
     [SerializeField] private float acceleration;
     [SerializeField] private Transform lever;
     [SerializeField] private Transform steeringWheel;
-    [SerializeField] private float turnSpeed;
-    [SerializeField] private float rotationZSpeed;
-
-    [SerializeField] private float minZClamp, maxZClamp;
-
     [Tooltip("This divides the rotation of the wheel so you don't get a turnspeed of 180 degrees")]
     [SerializeField] private float divideRotation;
-    private Rigidbody _rb;
+    // -]
+
+    // [-
+    [Header("Automatic Variables")]
+    [SerializeField] private float automaticSpeedMultiplier;
+    [SerializeField] private Transform[] _wayPoints;
+    private Queue<Transform> _wayPointQueue = new Queue<Transform>();
+    [SerializeField] private Vector2 _wayPointOffsetWidth;
+    [SerializeField] private Vector2 _wayPointOffsetLengthMinMax;
+    private Transform _lastPlacedWaypoint;
+    [SerializeField] private float tiltIntensity;
+
+    // -]
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody>();
+        if (movementType == BoatMovementType.Automatic)
+        {
+            foreach (Transform point in _wayPoints)
+            {
+                _wayPointQueue.Enqueue(point);
+            }
+            _lastPlacedWaypoint = _wayPoints[_wayPoints.Length - 1];
+            StartCoroutine(MoveAlongPath());
+        }
     }
 
-    
+    #region Manual
     private void FixedUpdate()
     {
+        if (movementType == BoatMovementType.Automatic) { return; }
         CalculateVelocity();
 
         //if (Mathf.Abs(acceleration) < 0.1f) return;
@@ -37,7 +71,7 @@ public class BoatMovement : MonoBehaviour
 
         CalculateZRotation();
 
-        
+
         transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime);
     }
 
@@ -63,7 +97,7 @@ public class BoatMovement : MonoBehaviour
 
         _rb.velocity = transform.forward * acceleration * speedMultiplier * Time.fixedDeltaTime;
 
-         
+
         if (leverAngle > -2 && leverAngle < 2)
         {
             acceleration = Mathf.Lerp(acceleration, 0, decelerationSpeed * Time.deltaTime);
@@ -96,7 +130,7 @@ public class BoatMovement : MonoBehaviour
     }
 
     private void CalculateZRotation()
-    {   
+    {
         Vector3 currentRotation = transform.eulerAngles;
         float zAngle = currentRotation.z;
 
@@ -112,9 +146,58 @@ public class BoatMovement : MonoBehaviour
             zAngle = Mathf.Lerp(zAngle, 0, Time.deltaTime * 2);
         }
 
-        
+
         zAngle = Mathf.Clamp(zAngle, minZClamp, maxZClamp);
 
         transform.eulerAngles = new Vector3(0, currentRotation.y, zAngle);
     }
+    #endregion
+
+    #region Automatic
+    private void AssignNewWaypointPosition(Transform wayPoint)
+    {
+        wayPoint.position = _lastPlacedWaypoint.position + new Vector3(
+            Random.Range(_wayPointOffsetWidth.x, _wayPointOffsetWidth.y),
+            _lastPlacedWaypoint.position.y,
+            Random.Range(_wayPointOffsetLengthMinMax.x, _wayPointOffsetLengthMinMax.y)
+        );
+        _lastPlacedWaypoint = wayPoint;
+    }
+
+    private IEnumerator MoveAlongPath()
+    {
+        acceleration = speedMultiplier * automaticSpeedMultiplier;
+        if (_wayPointQueue.Count == 0 || _rb == null) yield return null;
+
+        while(movementType == BoatMovementType.Automatic && _wayPointQueue.Count > 0)
+        {
+            var wayPoint = _wayPointQueue.Peek();
+            while (Vector3.Distance(transform.position, wayPoint.position) > 1f)
+            {
+                Vector3 direction = (wayPoint.position - transform.position).normalized;
+
+                _rb.MovePosition(_rb.position + direction * acceleration * Time.deltaTime * speedMultiplier * automaticSpeedMultiplier);
+
+                Quaternion targetRotation = Quaternion.LookRotation(wayPoint.position - transform.position);
+
+                Vector3 cross = Vector3.Cross(transform.forward, direction);
+                float tiltAmount = Mathf.Clamp(cross.y, -1f, 1f) * tiltIntensity;
+
+                Quaternion bankRotation = targetRotation * Quaternion.Euler(0, 0, -tiltAmount);
+
+                _rb.MoveRotation(Quaternion.Slerp(_rb.rotation, bankRotation, turnSpeed * Time.deltaTime));
+
+                yield return new WaitForEndOfFrameUnit();
+            }
+
+
+            AssignNewWaypointPosition(wayPoint);
+            _wayPointQueue.Enqueue(wayPoint);
+            _wayPointQueue.Dequeue();
+
+            yield return new WaitForEndOfFrameUnit();
+        }
+    }
+
+    #endregion
 }
